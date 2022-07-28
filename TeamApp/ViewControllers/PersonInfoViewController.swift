9dec2b532cc8ff75268171efd601b2e22320bc36
Durov Dmitrii <durov-dmitrii@mail.ru>
2022-07-28 18:38:38 +0300

    Add PersonInfoViewController

---
+++ TeamApp/ViewControllers/PersonInfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PersonInfoViewController.swift
+//  TeamApp
+//
+//  Created by Дмитрий Дуров on 28.07.2022.
+//
+
+import UIKit
+
+class PersonInfoViewController: UIViewController {
+    
+    @IBOutlet weak var photoEmployeeImage: UIImageView!
+    @IBOutlet weak var fullNameLabel: UILabel!
+    @IBOutlet weak var positionLabel: UILabel!
+    
+    var person: Person!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        photoEmployeeImage.image = UIImage(named: person.photo)
+        photoEmployeeImage.layer.cornerRadius = 10
+        
+        fullNameLabel.text = person.fullName
+        fullNameLabel.font = UIFont.systemFont(ofSize: 40)
+        
+        positionLabel.text = person.position
+        positionLabel.font = UIFont.systemFont(ofSize: 30)
+    }
+}
